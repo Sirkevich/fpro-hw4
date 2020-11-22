5b38c14126122c4c271d272b69e44db3b7d99f1d
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-22 16:25:42 +0200

    do task3

---
+++ app.js
@@ -0,0 +1,10 @@
+const userNumber = +prompt('Введите число от 0 до 100, и я покажу все числа, которые будут ему кратны');
+
+let result;
+
+for (let i = 1; i < 101; i++) {
+
+    if ((result = i % userNumber) === 0) {
+        console.log(i);
+    }
+}
